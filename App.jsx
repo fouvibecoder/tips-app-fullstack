36fcef6c4472a8255d2001ce806a72e4d9e090e0
fouvibecoder <tasso1044@gmail.com>
2025-07-30 00:04:34 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>TIPS App - Envelope Entry</h1>
+      <p>Welcome to your fullstack dev journey.</p>
+    </div>
+  );
+}
+
+export default App;
